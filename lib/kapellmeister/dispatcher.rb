0e4bd3cd2092e758ea73a02ba9cae913d7b735e2
Michael Seleznev <mmseleznev@gmail.com>
2026-06-08 10:42:08 +0300

    fix

--- lib/kapellmeister/dispatcher.rb
+++ lib/kapellmeister/dispatcher.rb
@@ -58,30 +58,31 @@ class Kapellmeister::Dispatcher
   private
 
   def connection_by(method_name, path, data = {})
+    data = data.dup
     additional_headers = data.delete(:headers) || {}
     requests_data = data.delete(:request) || {}
-    data_json = data.blank? ? '' : data.to_json
-    additional_headers['Content-Length'] = requests_data.merge(data).to_s.bytesize unless get?(method_name)
+    data_json = json_body(data)
+    additional_headers['Content-Length'] = data_json.bytesize.to_s unless get?(method_name)
 
     generated_connection = connection(additional_headers: additional_headers, requests_data: requests_data) # rubocop:disable Style/HashSyntax (for support ruby 2.4+)
 
-    process generated_connection.run_request(method_name.downcase.to_sym,
-                                             url_with_params(path, data, method_name),
-                                             data_json,
-                                             additional_headers)
-
+    process run_request(generated_connection, method_name, path, data, data_json, additional_headers)
   rescue Faraday::ConnectionFailed, Faraday::TimeoutError => e
     failed_response(details: e.message)
   end
 
   def connection(additional_headers:, requests_data:)
+    additional_headers = additional_headers.dup
+    additional_headers['Content-Type'] ||= 'application/json; charset=utf-8'
+
     request = requests_generate(**requests_data)
     headers = headers_generate(**additional_headers)
-    @connection ||= ::Faraday.new(url: configuration.url, headers: headers, request: request) do |faraday| # rubocop:disable Style/HashSyntax (for support ruby 2.4+)
-      faraday.request :json, content_type: 'application/json; charset=utf-8'
+    headers = normalize_headers(headers)
+
+    ::Faraday.new(url: configuration.url, headers: headers, request: request) do |faraday| # rubocop:disable Style/HashSyntax (for support ruby 2.4+)
+      faraday.request :json
       faraday.request :multipart if MULTIPART_AVAILABLE
       faraday.response :logger, logger
-      faraday.response :json, content_type: 'application/json; charset=utf-8'
       faraday.response :follow_redirects if FOLLOW_REDIRECTS_AVAILABLE
 
       if TYPHOEUS_ADAPTER_AVAILABLE
@@ -89,19 +90,45 @@ class Kapellmeister::Dispatcher
           http.timeout = 20
         end
       else
-        # Faraday has multiple adapters depending on the Faraday version.
-        # Default to :net_http when typhoeus adapter isn't available.
         faraday.adapter :net_http do |http|
           if http.respond_to?(:timeout=)
             http.timeout = 20
-          elsif http.respond_to?(:options) && http.options.respond_to?(:timeout=)
-            http.options.timeout = 20
-          elsif http.respond_to?(:options) && http.options.respond_to?(:[]=)
-            http.options[:timeout] = 20
+          else
+            http.read_timeout = 20 if http.respond_to?(:read_timeout=)
+            http.open_timeout = 20 if http.respond_to?(:open_timeout=)
+            http.write_timeout = 20 if http.respond_to?(:write_timeout=)
           end
         end
       end
     end
+  end
+
+  def normalize_headers(headers)
+    headers.each_with_object({}) do |(key, value), result|
+      next if value.nil?
+
+      result[key.to_s] =
+        if value.is_a?(Array)
+          value.compact.map(&:to_s).join(',')
+        elsif value.is_a?(String)
+          value
+        else
+          value.to_s
+        end
+    end
+  end
+
+  def json_body(data)
+    data.blank? ? '' : data.to_json
+  end
+
+  def run_request(connection, method_name, path, data, data_json, additional_headers)
+    connection.run_request(
+      method_name.downcase.to_sym,
+      url_with_params(path, data, method_name),
+      data_json,
+      additional_headers
+    )
   end
 
   def headers_generate(**additional)
